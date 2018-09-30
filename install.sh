dab795724b2121cf7b5dcced429b76301c384e9b
王爵 nice <biezhi.me@gmail.com>
2018-09-30 18:29:34 +0800

    :egg: add install script

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+get_latest_release() {
+  curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
+    grep '"tag_name":' |                                            # Get tag line
+    sed -E 's/.*"([^"]+)".*/\1/'                                    # Pluck JSON value
+}
+
+get_latest_release "biezhi/make-money"
+tar -zxvf make-money.tar.gz
+cd make-money
+sh tool start
