987afd061c950f107c62b7e7448f930bab2a36d3
王爵 nice <biezhi.me@gmail.com>
2018-09-30 21:29:14 +0800

    :package: update install script

--- install.sh
+++ install.sh
@@ -1,22 +1,18 @@
 #!/bin/sh
 
+APP_NAME="profit"
 get_latest_release() {
-  curl --silent "https://api.github.com/repos/$1/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/'
+  curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
+    grep '"tag_name":' |                                            # Get tag line
+    sed -E 's/.*"([^"]+)".*/\1/'                                    # Pluck JSON value
 }
 
-https://api.github.com/repos/biezhi/profit/releases/latest
+TAG_VERSION=$(get_latest_release "biezhi/profit")
 
-LATEST_RELEASE=$(curl -L -s -H 'Accept: application/json' https://github.com/biezhi/make-money/releases/latest)
+wget -N --no-check-certificate https://github.com/biezhi/profit/releases/download/$TAG_VERSION/profit.tar.gz
 
+echo '下载完毕'
 
-getLatest() {
-  REPO="biezhi/make-money"
-  SUFFIX="tar.gz"
-  curl --silent https://api.github.com/repos/$REPO/releases/latest | \
-    python -c "import sys; from json import loads as l; x = l(sys.stdin.read()); print(''.join(s['browser_download_url'] for s in x['assets'] if s['name'].endswith('.$SUFFIX')))"
-}
-
-get_latest_release "biezhi/make-money"
-tar -zxvf make-money.tar.gz
-cd make-money
+mkdir $APP_NAME
+tar -zxvf $APP_NAME.tar.gz -C $APP_NAME && cd $APP_NAME
 sh tool start
